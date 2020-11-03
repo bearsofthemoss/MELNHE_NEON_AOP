cc8055534f11e35225c16baed1baa6cfa06412f6
Alex Young <bearsofthemoss@gmail.com>
2020-11-03 08:55:00 -0500

    write tiffs for Figure 2

--- improvements to fig1.R
+++ improvements to fig1.R
@@ -51,9 +51,9 @@ byTileAOP("DP3.30010.001", site="BART", year="2017", check.size = F,buffer = 200
 
 pic.C7<-stack("data_folder\\DP3.30010.001\\2017\\FullSite\\D01\\2017_BART_3\\L3\\Camera\\Mosaic\\2017_BART_3_315000_4880000_image.tif")
 C7<-stands[stands$stand=="C7",]
-control<-C7[C7$plot=="1", ]
+control<-C7[C7$plot=="3", ]
 control3<-extent(control)+10
-co.bart<-crop(pic.C3, control3)
+co.bart<-crop(pic.C7, control3)
 
 
 
@@ -73,83 +73,19 @@ mini<-extent(control3)
 mini_noshade <- crop(cube_no_shade,mini)
 
 ############################
-par(mfrow=c(1,3))
+par(mfrow=c(1,1))
 #3333 Rgb image
 plotRGB(co.bart)
-
+dpi=300
+#tiff("R_output/tree_fig",width=12*dpi, height=10*dpi, res=dpi)
+dev.off()
 # add treetops to chm
-plot(chm7, axes=F, box=F, legend=F)
-plot(C7crownsPoly, border = "white", lwd = 0.5, add = TRUE)
-plot(m7tops, add=T, pch=17,col="red", cex=1.5)
+#tiff("R_output/lidar_fig",width=12*dpi, height=10*dpi, res=dpi)
+plot(chm7, axes=F, box=F, legend=T)
+#plot(C7crownsPoly, border = "white", lwd = 0.5, add = TRUE)
+plot(m7tops, add=T, pch=21,bg="blue",col="white", cex=1.5)
 
 plotRGB(mini_noshade, r = 56, g = 28, b = 14, stretch = 'lin')
-plot(C7crownsPoly, border = "white", lwd = 0.5, add = TRUE)
-plot(m7tops, add=T, pch=17, col="red",cex=1.5)
-
-
-
-#################################################################################################################################
-## read in data, add 'ages', add 'YesN','NoN' for N*P ANOVA
-dada<-read.csv("actual_tops_10_04_greater_0.1.csv")
-dada<-dada[,-1]
-
-# stand ages
-dada$Age[dada$Stand=="C1"]<-"~30 years old"
-dada$Age[dada$Stand=="C2"]<-"~30 years old"
-dada$Age[dada$Stand=="C3"]<-"~30 years old"
-dada$Age[dada$Stand=="C4"]<-"~60 years old"
-dada$Age[dada$Stand=="C5"]<-"~60 years old"
-dada$Age[dada$Stand=="C6"]<-"~60 years old" 
-dada$Age[dada$Stand=="C7"]<-"~100 years old"
-dada$Age[dada$Stand=="C8"]<-"~100 years old"
-dada$Age[dada$Stand=="C9"]<-"~100 years old"
-
-
-
-names(dada)
-
-# make a 'long' version of dada
-ldada<-gather(dada, "wvl","refl",7:351)
-ldada$wvl<-as.numeric(gsub(".*_","",ldada$wvl))
-ldada<-na.omit(ldada) # take out NA values- about half were NA 10_3 Ary
-ldada$staplo<-paste(ldada$Stand, ldada$Treatment)
-
-
-# look at number of obs per plot
-table(ldada$Treatment, ldada$Stand)/345  
-table(is.na(ldada$refl), ldada$Treatment) # but alot are NA
-
-# min,max, and mean number of tree tops by plot.  6 is probably too low right?
-min(table(ldada$staplo))/345
-max(table(ldada$staplo))/345
-mean(table(ldada$staplo))/345
-
-dim(ldada)
-
-# view individual trees
-# Here Alex tried to visualize the spectra---
-
-
-li<-subset(ldada, ldada$wvl<=1340)
-sw<-subset(ldada, ldada$wvl>=1445 & ldada$wvl<=1790)
-ir<-subset(ldada, ldada$wvl>=1995)
-# check the values
-li$group<-"1"
-sw$group<-"2"
-ir$group<-"3"
-#
-table(li$wvl)
-table(sw$wvl)
-table(ir$wvl)
-# move forward without 'band' bands
-ldada<-rbind(li,sw,ir)
-ldada$tree<-paste(ldada$Stand, ldada$Treatment, ldada$treeID)
-ldada$group.tree<-paste(ldada$tree, ldada$group)
-
-# Just view 1 stand
-C1_ldada<-ldada[ldada$staplo=="C3 Control",]
-
-# Nice!
-ggplot(C1_ldada, aes(x=wvl,col=Stand,group=group.tree, y=refl))+geom_line()
-
-
+#plot(C7crownsPoly, border = "white", lwd = 0.5, add = TRUE)
+plot(m7tops, add=T, pch=21,bg="blue",  col="white",cex=1.5)
+tiff("R_output/shade_fig",width=12*dpi, height=10*dpi, res=dpi)
